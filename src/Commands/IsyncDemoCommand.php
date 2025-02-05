94b945a27f853cf4a74091db92668a4cb5410893
tusharkuhite <tusharkuhite123@gmail.com>
2025-02-05 15:01:16 +0530

    new commit tag 1.4.0

--- src/Commands/IsyncDemoCommand.php
+++ src/Commands/IsyncDemoCommand.php
@@ -3,6 +3,7 @@
 namespace Isync\Demo\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class IsyncDemoCommand extends Command
 {
@@ -25,6 +26,29 @@ class IsyncDemoCommand extends Command
         $this->call('db:seed', [
             '--class' => 'Isync\\Demo\\Database\\Seeders\\DatabaseSeeder'
         ]);
+
+        $publishableFiles = [
+            __DIR__ . '../models' => app_path('Models'),
+            __DIR__ . '../Controllers' => app_path('Http/Controllers'),
+            __DIR__ . '../Middleware/' => app_path('Http/Middleware/'),
+            __DIR__ . '../views/layouts' => resource_path('views/layouts'),
+            __DIR__ . '../views/admin' => resource_path('views/admin'),
+            __DIR__ . '../Assets/' => public_path(),
+            __DIR__ . '../General/' => app_path('Libraries/'),
+            __DIR__ . '../routes/generatedRoute.php' => base_path('routes/web.php'),
+            __DIR__ . '../models/User.php' => app_path('Models/User.php')
+        ];
+
+
+        foreach (array_keys($publishableFiles) as $fileOrDir) {
+            if (File::exists($fileOrDir)) {
+                File::delete($fileOrDir);
+            }
+
+            if (File::isDirectory($fileOrDir)) {
+                File::deleteDirectory($fileOrDir);
+            }
+        }
     
         $this->info('Hello Isync Developer! The setup is complete.');
     }
