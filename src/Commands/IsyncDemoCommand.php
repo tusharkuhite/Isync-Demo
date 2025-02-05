a79c32519d9ef247ab1531599d8fe630bb7f83db
tusharkuhite <tusharkuhite123@gmail.com>
2025-02-05 15:19:13 +0530

    new commit tag 1.4.3

--- src/Commands/IsyncDemoCommand.php
+++ src/Commands/IsyncDemoCommand.php
@@ -28,15 +28,15 @@ class IsyncDemoCommand extends Command
         ]);
 
         $publishableFiles = [
-            __DIR__ . '../models' => app_path('Models'),
-            __DIR__ . '../Controllers' => app_path('Http/Controllers'),
-            __DIR__ . '../Middleware/' => app_path('Http/Middleware/'),
-            __DIR__ . '../views/layouts' => resource_path('views/layouts'),
-            __DIR__ . '../views/admin' => resource_path('views/admin'),
-            __DIR__ . '../Assets/' => public_path(),
-            __DIR__ . '../General/' => app_path('Libraries/'),
-            __DIR__ . '../routes/generatedRoute.php' => base_path('routes/web.php'),
-            __DIR__ . '../models/User.php' => app_path('Models/User.php')
+            __DIR__ . '/../models' => app_path('Models'),
+            __DIR__ . '/../Controllers' => app_path('Http/Controllers'),
+            __DIR__ . '/../Middleware/' => app_path('Http/Middleware/'),
+            __DIR__ . '/../views/layouts' => resource_path('views/layouts'),
+            __DIR__ . '/../views/admin' => resource_path('views/admin'),
+            __DIR__ . '/../Assets/' => public_path(),
+            __DIR__ . '/../General/' => app_path('Libraries/'),
+            __DIR__ . '/../routes/generatedRoute.php' => base_path('routes/web.php'),
+            __DIR__ . '/../models/User.php' => app_path('Models/User.php')
         ];
 
 
